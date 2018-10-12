fbba2ee452bc01bf08844bad01f3fc937ec2bccc
Efutei <hirofunafumi@gmail.com>
2018-10-13 02:14:49 +0900

    initialize notes

--- js/main-scene.js
+++ js/main-scene.js
@@ -11,7 +11,7 @@ phina.define('MainScene', {
       this.can = Can().addChildTo(this);
       // 本社ノーツ作成
       this.honsha = Honsha().addChildTo(this);
-  
+
       var self = this;
       this.can.onpointstart = function(){
         self.can.doAction(true);
@@ -19,6 +19,27 @@ phina.define('MainScene', {
       this.honsha.onpointstart = function(){
         self.honsha.doAction(true);
       }
+      this.notes = [];
+      scoreManager.load("score1").then((data) => {
+        console.log(data);
+        this.song = data;
+        this.song.audio.play();
+        data.score.forEach((item) => {
+          notePositionX = self.gorilla.x + item.time/3;
+          notePositionY = 200;
+          //缶の場合
+          if(item.type == 1){
+            newCan = Can().addChildTo(this);
+            newCan.setPosition(notePositionX,notePositionY);
+            self.notes.push(newCan);
+          //本社の場合
+          }else if(item.type == 2){
+            newHonsha = Honsha().addChildTo(this);
+            newHonsha.setPosition(notePositionX,notePositionY);
+            self.notes.push(newHonsha);
+          }
+        });
+      });
     },
     //毎フレーム実行されるメソッド
     update: function(app) {
@@ -28,6 +49,11 @@ phina.define('MainScene', {
         //デバック用
         this.gorilla.action();
       }
+      this.notes.forEach((item) => {
+        item.x -= 30;
+        // if(this.song.audio.currentTime == item.timing) {
+        //
+        // }
+      });
     },
   });
-  
