105afe2c7fd02e278eb4793cec0a93ad915f2c7e
mickeyzwo <singing.programmer@gmail.com>
2018-10-16 17:36:46 +0900

    譜面マネージャを改良，キャッシュ機能でサクサク動くゾイ

--- js/score-manager.js
+++ js/score-manager.js
@@ -3,22 +3,16 @@ const scoreManager = (_ => {
     "use strict";
 
     const scores = [];
-
+    const cache = {};
     const add = json => scores.push(json);
 
-    let audio = null;
-    let score = null;
-    let bpm = 120;
-    let measure = 48;
-    let delay = 1000;
-
     const parseScore = json => {
-        bpm = json.bpm;
-        measure = 48 * eval(json.measure);
-        delay = json.delay;
-        const score = json.score;
         const result = [];
-        let now = delay;
+        const score = json.score;
+        let bpm = json.bpm;
+        const m = json.measure.split("/");
+        let measure = 48 * (+m[0] / +m[1]);
+        let now = json.delay;
         let def = 60000 / bpm / 12;
         score.forEach(bar => {
             let res = "";
@@ -40,14 +34,14 @@ const scoreManager = (_ => {
     const load = async title => {
         const target = scores.find(obj => obj.title == title);
         if (!target) throw new Error("song is not exist!");
-        audio = await mediaLoader.loadAudio("./sounds/" + target.song);
-        score = parseScore(target);
+        if (cache[title]) return cache[title];
+        const audio = await mediaLoader.loadAudio("./sounds/" + target.song);
+        const score = parseScore(target);
+        cache[title] = { audio, score };
         //console.log(score);
         return { audio, score };
     };
 
-    return {
-        add, load
-    };
+    return { add, load };
 
 })();
